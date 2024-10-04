ccd7e998aedac0299186cebbc6a6f5a8c7837a33
Amirhossein-Ra <000amir111rahzany@gmail.com>
2024-10-04 23:39:34 +0330

    making category

--- src/app/Components/TopNav.jsx
+++ src/app/Components/TopNav.jsx
@@ -4,11 +4,10 @@ import { FiShoppingCart } from "react-icons/fi";
 import { CiSearch } from "react-icons/ci";
 export default function TopNav() {
   return (
-    <div className="flex items-center justify-between space-x-4 py-3 px-2">
-      <div className="flex items-center space-x-3 mr-[400px]">
-        <div className="">
-          <FiShoppingCart size={23} />
-        </div>
+    <div className=" flex items-center justify-between space-x-4 py-3 px-2">
+      <div className="hidden sm:inline-flex  items-center space-x-3 mr-[400px]">
+        <FiShoppingCart size={23} />
+
         <div className="flex items-center border border-gray-600 p-2 space-x-1 rounded-md">
           <span className="text-sm font-semibold border-r-2 border-gray-600 px-2">
             ثبت نام
@@ -17,9 +16,17 @@ export default function TopNav() {
           <TbLogin size={23} />
         </div>
       </div>
-      <div className="w-[40%] flex items-center ltr:flex-1 py-2 px-1 rounded-md bg-[#f0f0f1] space-x-1">
+      <div className="w-[95%] sm:w-[40%] flex items-center sm:justify-normal justify-end ltr:flex-1 py-2 px-1 sm:rounded-md rounded-full bg-[#f0f0f1] space-x-1">
+        <div className="flex-1 sm:hidden inline-flex">
+          <img
+            className="h-5 "
+            src="./images/typography.png"
+            alt="search image"
+          />
+        </div>
+
         <input
-          className="flex-1 bg-[#f0f0f1]  placeholder:text-gray-600 placeholder:text-sm py-2"
+          className="flex sm:flex-1 bg-[#f0f0f1]  placeholder:text-gray-600 placeholder:text-sm py-2"
           dir="rtl"
           type="text"
           placeholder="جستوجو"
@@ -27,10 +34,11 @@ export default function TopNav() {
         <CiSearch size={20} />
       </div>
 
-      <div className=""></div>
-      <div className="">
-        <img className="h-10" src="./images/full-horizontal.png" alt="Logo" />
-      </div>
+      <img
+        className="hidden sm:inline-flex h-10"
+        src="./images/full-horizontal.png"
+        alt="Logo"
+      />
     </div>
   );
 }
